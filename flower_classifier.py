044cf90a2953c011a25d5c82524d6e14b4950d5a
Aniket Limaye <allimaye@edu.uwaterloo.ca>
2018-02-07 02:10:30 -0500

    Implemented majority vote for K nearest neighbors

--- flower_classifier.py
+++ flower_classifier.py
@@ -5,7 +5,11 @@ from sklearn.metrics import accuracy_score
 from scipy.spatial import distance
 import heapq
 
-class KNNClassifer():
+class KNNClassifier():
+
+    def __init__(self):
+        pass
+
     def fit(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
@@ -15,9 +19,9 @@ class KNNClassifer():
             k = 1
         predictions = []
         for row in x_test:
-            prediction = self.closest(row)
+            prediction = self.closest(row, k)
             predictions.append(prediction)
-        return prediction
+        return predictions
 
     def closest(self, row, k):
         # Use a max heap to store k closest points.
@@ -25,14 +29,23 @@ class KNNClassifer():
         for i in xrange(len(self.x_train)):
             dist = distance.euclidean(row, x_train[i])
             if (len(max_heap) < k):
-                heapq.heappush(max_heap, (x_train[i], dist * -1, i))
+                heapq.heappush(max_heap, (dist * -1, i))
                 continue
-            cmp_dist = max_heap[0][1] * -1
+            cmp_dist = max_heap[0][0] * -1
             if (dist < cmp_dist):
                 heapq.heappop(max_heap)
-                heapq.heappush(max_heap, (x_train[i], dist * -1, i))
+                heapq.heappush(max_heap, (dist * -1, i))
 
-        return 0
+        # take majority vote from k closest points
+        tally, most_popular, count = {}, None, 0
+        for dist, index in max_heap:
+            label = y_train[index]
+            tally[label] = tally.get(label, 0) + 1
+            if (tally[label] > count):
+                count = tally[label]
+                most_popular = label
+
+        return most_popular
 
 
 
@@ -43,10 +56,10 @@ labels = iris.target
 
 x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size= 0.3)
 
-KNN_classifier = KNeighborsClassifier()
+KNN_classifier = KNNClassifier()
 
 KNN_classifier.fit(x_train, y_train)
+predictions = KNN_classifier.predict(x_test, 2)
 
-predictions = KNN_classifier.predict(x_test)
 print accuracy_score(y_test, predictions)
 
