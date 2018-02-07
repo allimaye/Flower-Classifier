b53e169b41f3adaf5871665f04f318e8dd1d5abc
Aniket Limaye <allimaye@edu.uwaterloo.ca>
2018-02-07 01:10:16 -0500

    Added custom KNN classifier interface

--- flower_classifier.py
+++ flower_classifier.py
@@ -2,6 +2,27 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from scipy.spatial import distance
+import heapq
+
+class KNNClassifer():
+    def fit(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def predict(self, x_test, k):
+        if(k <= 0 or k is None):
+            k = 1
+        predictions = []
+        for row in x_test:
+            prediction = self.closest(row)
+            predictions.append(prediction)
+        return prediction
+
+    def closest(self, row, k):
+        pass
+
+
 
 iris = datasets.load_iris()
 
